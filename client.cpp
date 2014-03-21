08839e6d8692e1990f5413b25668f59908b82a5a
Clay Miller <clay@smockle.com>
2014-03-20 23:32:15 -0500

    Include cstdlib and use std::atof.

--- client.cpp
+++ client.cpp
@@ -2,19 +2,18 @@
 
 using namespace dicey;
 
-std::string ip_address
-int prob_loss;
-int prob_corrupt;
+std::string ip_address;
+double prob_loss;
+double prob_corrupt;
 std::string filename;
 
 int main(int argc, char* argv[]) {
-    std::cout << "<ip_address> <loss_probability> <corrupt_probability> <filename>" << std::endl;
-    std::cout << argc << std::endl;
-
     ip_address = argc > 1 ? argv[1] : "127.0.0.1";
-    prob_loss = argc > 2 ? argv[2] : 0;
-    prob_corrupt = argc > 3 ? argv[3] : 0;
+    prob_loss = argc > 2 ? std::atof(argv[2]) : 0;
+    prob_corrupt = argc > 3 ? std::atof(argv[3]) : 0;
     filename = argc > 4 ? argv[4] : "TestFile";
 
+    std::cout << "dicey " << ip_address << " " << prob_loss << " " << prob_corrupt << " " << filename << std::endl;
+    
     return 0;
 }
--- client.h
+++ client.h
@@ -1,8 +1,10 @@
 #ifndef CLIENT_H
 #define CLIENT_H
 
+#include <cstdlib>
 #include <stdio.h>
 #include <iostream>
+#include <sys/socket.h>
 
 namespace dicey {
 }
