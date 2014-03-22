606148e5c5661495928d485130b5665a01de67d5
Clay Miller <clay@smockle.com>
2014-03-22 00:00:18 -0500

    Duplicate members in header file and cpp file.

--- packet.cpp
+++ packet.cpp
@@ -2,7 +2,19 @@
 
 using namespace dicey;
 
-class Packet::Packet {
+class Packet {
+    // Order in the sequence of sent packets. (0 or 1)
+    bit seq_num;
+
+    // Whether the packet was received successfully/acknowledged. (0/nak or 1/ack)
+    bit ack;
+
+    // Checksum based on the packet's data.
+    ush checksum;
+
+    // 128 bytes of data.
+    char data[PACKET_SIZE];
+    
     Packet(bit seq_num, char data[PACKET_SIZE]) {
         this->seq_num = seq_num;
         this->ack = 0;
